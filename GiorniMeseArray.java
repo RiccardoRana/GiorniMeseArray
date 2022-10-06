4fc5b88709972a53981ea34752e1619e11d9c0a7
RiccardoRana <88197051+RiccardoRana@users.noreply.github.com>
2022-10-06 10:50:52 +0200

    Correzione 2

--- GiorniMeseArray.java
+++ GiorniMeseArray.java
@@ -9,7 +9,7 @@ public class GiorniMeseArray {
 		System.out.print("Inserisci un numero da 1 a 12 e ti dirò il mese corrispondente e il numero di giorni: ");
 		int meseScelto = in.nextInt();
 
-		System.out.println("Il mese di " + mesi[meseScelto - 1] + " ha " + giorni[meseScelto] + " giorni!");
+		System.out.println("Il mese di " + mesi[meseScelto - 1] + " ha " + giorni[meseScelto-1] + " giorni!");
 
 		in.close();
 	}
